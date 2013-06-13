215c5f49512bc707cdab5de8950f06cf9d22aff8
Ybalrid <ybalrid@cecmu.org>
2013-06-14 01:33:23 +0200

    Add documentation and comments

--- exampleUse.cpp
+++ exampleUse.cpp
@@ -1,15 +1,32 @@
+//C++ std include
 #include <iostream>
 #include <sstream>
 #include <string>
+
+//SDLo
 #include <SDLo>
 
-//This main is just for ilustration
+
+//This main is just for ilustration. it's not part of the lib itself
 
 using namespace SDLo; //if you're lasy, all SDLo is in this namespace... 
+
+//All SDLo componant is member of the SDLo namespace
 
 int main()
 {
     Screen screenObject; //Create Screen
+    
+    /*  Screen is the main class of SDLo,
+     *  This is by a object Screen that you
+     *  will be able to display stuff on 
+     *  youre actual screen.
+     *
+     *  You can set many parameters before 
+     *  starting the video system. there is some
+     *  of them that you cannot modify after the 
+     *  video started.
+     */
 
     screenObject.setWidth(500); //set geometry
     screenObject.setHeight(500); 
@@ -17,6 +34,23 @@ int main()
     screenObject.startVideo(); //Launch SDL video subsystem    
     screenObject.setTitle("Test SDL++"); //Set window Title 
     
+    
+    /*  The second most important element is
+     *  the Sprite. With this class, you can display
+     *  fixed or animated objects on the Screen.
+     *
+     *  The animation are time based. This is pretty simple,
+     *  you load as many imagies you need on your sprite object.
+     *  You define the bounds (witch are images index on the order
+     *  you load them) of the animation loop and a speed on fps 
+     *  ("frames per seconds"). 
+     *
+     *  All sprites with "canBeDisplayed" set to 'true' present on
+     *  the screenObject "render list" will be displayed on screen.
+     *  
+     *  The positions of the sprites are relatives to the upper-left
+     *  corner of the Screen.
+     */
 
     Sprite* perso = screenObject.addSprite(new Sprite); //creating a SDLo::Sprite  object
     
@@ -36,20 +70,29 @@ int main()
     //this is a walkcycle of 6 images, so we load the 6 frames on the Sprite object
     for(int i = 0; i < 6; i++)
     {
+        //I use this technic to load a bunch of images
         std::stringstream file;
         file <<"ressources/tux_walk/walk-" << i << ".png";
-        std::cerr << "Loading file :" << file.str() << std::endl;
 
         walkTux->addFrame(IMG_Load(file.str().c_str()));
     } //I have named my files to be easily lodable this way
     
     //lets configure the sprite
-    walkTux->setCanBeDisplayed(true);
+    walkTux->setCanBeDisplayed();
     walkTux->setFps(20);//speed of the animation
     walkTux->setAnimBounds(0,5); //bounds of the loop, you can have as many animation you want, just load the frames in the order you want
     //remember, the first frame is "0", not "1", so here the last frame is 5, not 6;
     walkTux->setAnimated(); //if you dont set your sprite in the "animated state", it will not play the animation
 
+    /*  The principal code of your aplication will be on a loop like this one.
+     *
+     *  In order to use the integreted event system, you have to call the 'testEvents()'
+     *  method. You will no be able to recive the updated event information 
+     *  from yout Screen object if you don't do that.
+     *  
+     *  While your Screen dosent request to terminate the programm (alive = false),
+     *  you have to call the 'updateVideo()' method to refresh the screen.
+     */
 
     //render loop
     while(screenObject.isAlive())
